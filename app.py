e22116138186f10cba157bc48e8d6e8525567020
krishnazen27 <79613322+krishnazen27@users.noreply.github.com>
2026-07-12 00:55:32 +0100

    add templates to serve index file https://share.gemini.google/fPow9rjXEeKz
    Serve index.html via render_template instead of plain text

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def serve_frontend_page():
-    return "Hello, Classifieds!"
-
+    return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
