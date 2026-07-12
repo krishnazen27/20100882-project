0d916e12cc88b20ded1b2a5c34cb771db5539fe4
krishnazen27 <79613322+krishnazen27@users.noreply.github.com>
2026-07-12 01:12:28 +0100

    Imported and Added SQLite connection helper
    Imported and Added SQLite connection helper

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+import sqlite3
 from flask import Flask, render_template
 from flask_cors import CORS
 
 app = Flask(__name__, template_folder='templates')
 CORS(app, supports_credentials=True)
+
+DB_FILE = 'classifieds.db'
+
+def get_db_connection():
+    conn = sqlite3.connect(DB_FILE)
+    conn.row_factory = sqlite3.Row
+    return conn
+
 
 @app.route('/')
 def serve_frontend_page():
